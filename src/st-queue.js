90bf5d3b32ef757f1e504949ddcbd7d11e37b3cf
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-10-19 01:38:33 +0300

    feat:add solution Queue tasck

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 module.exports = class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    this.front = null;
+   
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+   return this.front
+  }
+  isEmpty(){
+    return !this.front
+  }
+
+  enqueue( value) {
+   
+    if (this.isEmpty()) {
+      this.front = new ListNode(value)
+      return;
+    }else{
+      let node = this.front;
+      while(node.next)node = node.next;
+      node.next = new ListNode(value)
+    }
+   
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
+    if(this.isEmpty()){
+      return undefined
+    }
+   let value = this.front.value;
+   this.front = this.front.next;
+   return value
   }
 
 }
