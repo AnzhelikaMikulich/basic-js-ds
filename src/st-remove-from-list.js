70392f24a0d0dabe9edcf94015945117dd234b02
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2021-10-18 17:00:58 +0300

    feat:add solution Linked List task

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList(l, k) {
+  let currentNode = l;
+  if (l.value === k){
+     l = l.next;
+  }
+  while (currentNode.next != null) {
+    if (currentNode.next.value == k){
+      currentNode.next = currentNode.next.next;
+    } 
+    currentNode = currentNode.next;
+  }
+  return l
 }
